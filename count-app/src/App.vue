af6da336c793f1651a0ab60d2a727d5aa8c99b5b
katyjohn124 <148299728+katyjohn124@users.noreply.github.com>
2023-12-08 16:35:40 +0800

    initial commit

---
+++ count-app/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+// ref用于定义响应式数据
+const count = ref(0)
+</script>
+
+
+
+<template>
+  <main>
+    <div>
+      <h4>the current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
+</template>
+
+
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  display: flex;
+
+  align-items: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
